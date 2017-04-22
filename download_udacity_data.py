0e0eb433380ebd0b3068d2d5ffa990e822ef862f
nghiattran <nghiattran3@gmail.com>
2017-04-21 22:24:54 -0400

    make udacity data consistent with kittibox

--- download_udacity_data.py
+++ download_udacity_data.py
@@ -7,6 +7,7 @@ from __future__ import division
 from __future__ import print_function
 
 import logging
+import shutil
 import subprocess
 import sys
 import os
@@ -16,6 +17,10 @@ from six.moves import urllib
 logging.basicConfig(format='%(asctime)s %(levelname)s %(message)s',
                     level=logging.INFO,
                     stream=sys.stdout)
+
+TRAINING_FOLDER = 'training'
+IMAGE_FOLDER = 'image_2'
+LABEL_FOLDER = 'label_2'
 
 datasets = {
     'crowdai': {
@@ -84,37 +89,45 @@ def handle_data(udacity_dec_dir, type):
         logging.info("Done extracting Udacity %s Data." % type)
 
     # If data is not extracted yet, extract
-    data_extected_folder = os.path.join(udacity_dec_dir, extracted_dirname)
-    if os.path.exists(data_extected_folder):
-        logging.info("Udacity %s extracted data FOUND at %s." % (type, data_extected_folder))
+    data_extracted_folder = os.path.join(udacity_dec_dir, extracted_dirname)
+    if os.path.exists(data_extracted_folder):
+        logging.info("Udacity %s extracted data FOUND at %s." % (type, data_extracted_folder))
     else:
         logging.info("Extracting Udacity %s Data." % type)
         tarfile.open(data_path, "r").extractall(udacity_dec_dir)
 
     # Convert Autti data to Kitti
     converted_path = os.path.join(udacity_dec_dir, type.lower())
-    if os.path.exists(converted_path):
-        logging.info("Udacity %s converted data FOUND at %s." % (type, converted_path))
-    else:
-        try:
-            logging.info("Converting Udacity %s Data." % type)
-            data_tar = os.path.join(udacity_dec_dir, extracted_dirname)
-            convert_to_kitti(data_tar, converted_path, 'udacity-%s' % type.lower())
-            logging.info("Done converting Udacity %s Data." % type)
-        except Exception as e:
-            logging.warning("Failed to convert Udacity %s to Kitti." % type)
-            logging.warning(
-                "Make sure you have all submodules installed by running: git submodule update --init --recursive")
-            exit(1)
+    try:
+        logging.info("Converting Udacity %s Data." % type)
+        data_tar = os.path.join(udacity_dec_dir, extracted_dirname)
+        convert_to_kitti(data_tar, converted_path, 'udacity-%s' % type.lower())
+        logging.info("Done converting Udacity %s Data." % type)
+    except Exception as e:
+        logging.warning("Failed to convert Udacity %s to Kitti." % type)
+        logging.warning(
+            "Make sure you have all submodules installed by running: git submodule update --init --recursive")
+        exit(1)
+
+    src = os.path.join(converted_path, TRAINING_FOLDER, IMAGE_FOLDER)
+    for file in os.listdir(src):
+        image_src = os.path.join(converted_path, TRAINING_FOLDER, IMAGE_FOLDER, file)
+        image_dst = os.path.join(udacity_dec_dir, TRAINING_FOLDER, IMAGE_FOLDER, file)
+        shutil.move(src=image_src,
+                    dst=image_dst)
 
     new_data = []
     with open(os.path.join(converted_path, 'train.txt'), 'r') as f:
         data = f.readlines()
         for id in data:
             id = id[:-1]
-            entry = '%s %s' % (os.path.join(type.lower(), 'training', 'image_2', id + '.jpg'),
-                               os.path.join(type.lower(), 'training', 'label_2', id + '.txt'))
+            entry = '%s %s' % (os.path.join(TRAINING_FOLDER, IMAGE_FOLDER, id + '.jpg'),
+                               os.path.join(TRAINING_FOLDER, LABEL_FOLDER, id + '.txt'))
             new_data.append(entry)
+
+    # Remove data to save space
+    shutil.rmtree(converted_path)
+    shutil.rmtree(data_extracted_folder)
 
     return new_data
 
@@ -131,14 +144,25 @@ def convert_to_kitti(from_path, to_path, type):
 if __name__ == '__main__':
     data_dir, run_dir = get_pathes()
 
+    # Create folder structure for dataset
     if not os.path.exists(data_dir):
         os.makedirs(data_dir)
 
     udacity_dec_dir = os.path.join(data_dir, 'Udacity')
-
     if not os.path.exists(udacity_dec_dir):
         os.makedirs(udacity_dec_dir)
 
+    training_folder = os.path.join(udacity_dec_dir, TRAINING_FOLDER)
+
+    image_folder = os.path.join(training_folder, IMAGE_FOLDER)
+    if not os.path.exists(image_folder):
+        os.makedirs(image_folder)
+
+    label_folder = os.path.join(training_folder, LABEL_FOLDER)
+    if not os.path.exists(label_folder):
+        os.makedirs(label_folder)
+
+    # Get data
     crowdai_data = handle_data(udacity_dec_dir, type='Crowdai')
     autti_data = handle_data(udacity_dec_dir, type='Autti')
 
